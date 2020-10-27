7dcdff583690ca07073c030973bcfcb58b51e180
Edmar-Albuquerque <edmar.albnascimento@gmail.com>
2020-10-27 15:48:29 -0400

    backend

--- web/src/pages/Landing.tsx
+++ web/src/pages/Landing.tsx
@@ -1,37 +1,33 @@
-import React from 'react'
-import { FiArrowRight} from 'react-icons/fi'
+import React from "react";
+import { FiArrowRight } from "react-icons/fi";
+import { Link } from "react-router-dom";
 
+import "../styles/pages/landing.css";
 
-import '../styles/pages/landing.css'
+import logoImg from "../images/logo.svg";
 
-import logoImg  from '../images/logo.svg'
-
-
-function Landing(){
-    return(
-     <div id="page-landing" >
+function Landing() {
+  return (
+    <div id="page-landing">
       <div className="content-wrapper">
-        <img src={logoImg} alt="Happy"/>
+        <img src={logoImg} alt="Happy" />
 
         <main>
           <h1>Leve felicidade para o mundo</h1>
-          <p>Visite orfanatos e mude o dia
-             de muitas crianças.
-          </p>
+          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
         </main>
 
         <div className="location">
-            <strong>Manaus</strong>
-            <span>Amazonas</span>
+          <strong>Manaus</strong>
+          <span>Amazonas</span>
         </div>
 
-        <a href="#" className="enter-app">
-            <FiArrowRight size={26} color="rgba(0,0,0,0.6)"/>
-        </a>
-
+        <Link to="/app" className="enter-app">
+          <FiArrowRight size={26} color="rgba(0,0,0,0.6)" />
+        </Link>
       </div>
     </div>
-    )
+  );
 }
 
 export default Landing;
